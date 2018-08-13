0e240e614754a9c6886305a9c28f73d8c2982ff9
Sérgio Ferreira Filho <sergio.ferreira.filho@gmail.com>
2018-08-13 19:35:47 -0300

    Inicial commit

---
+++ src/main/java/br/pucminas/api/SicadeApplication.java
@@ -0,0 +1,12 @@
+package br.pucminas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicadeApplication.class, args);
+	}
+}
