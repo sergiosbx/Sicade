acd5baf052e7f604382e13d4019befba7541e45f
Sérgio Ferreira Filho <sergio.ferreira.filho@gmail.com>
2018-10-05 00:28:56 -0300

    correcao migrate

--- src/main/resources/db/migration/postgres/V4__carga_usuario.sql
+++ src/main/resources/db/migration/postgres/V4__carga_usuario.sql
@@ -1,3 +1,4 @@
+ALTER TABLE usuario ALTER COLUMN senha TYPE character varying(250);
 DELETE FROM usuario WHERE email = 'adm@sicade.com.br';
 INSERT INTO usuario (email, senha, perfil) VALUES
 ('adm@sicade.com.br','$2a$10$mGENmHzO6auxgbLAXuixpOwQrxnmmOylY9uhHTQ0meLeP90FsFriq','ROLE_ADMIN');
